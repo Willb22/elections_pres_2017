73a303f9765d3713151f007cc4173117cc4608ee
will <williambaye22@gmail.com>
2020-11-20 13:56:27 +0100

    first commit

---
+++ src/communes/explore.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import  random
+import matplotlib.pyplot as plt
+import overpass
+#api = overpass.API()
+import datetime
+
+################### fichier pdf ##############################
+pp = PdfPages(' Exploration communes.pdf')
+firstPage = plt.figure(figsize=(11.69,8.27))
+firstPage.clf()
+txt = "Poucentage d'abstension par commune"
+firstPage.text(0.5,0.5,txt, transform=firstPage.transFigure, size=12, ha="center")
+pp.savefig()
+
+
+
+data = pd.read_csv('../../input_files/Presid_2017_Communes_Tour_1.csv')
+
+
+alist = list()
+absten = list()
+communes = list()
+inscrits = list()
+for i in range(100):
+    larger = data['% Abs/Ins'] > i
+    smaller = data['% Abs/Ins'] <= i+1
+    alist.append(data[larger & smaller])
+    #print(alist[i]['Inscrits'].sum())
+    absten.append(alist[i]['Abstentions'].sum())
+    inscrits.append(alist[i]['Inscrits'].sum())
+    #print(len(alist[i]), " communes ", i, "% d'abstention pour ", inscrits[i], " inscrits")
+
+    communes.append(len(alist[i]))
+
+plt.bar(range(100), communes)
+plt.title("Nombre de communes par pourcentage d'abstention")
+plt.ylabel('Nombre de communes')
+plt.xlabel('% Abs/Ins')
---
+++ src/communes/graphs.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+import  random
+import matplotlib.pyplot as plt
+import overpass
+#api = overpass.API()
+import re
+from string import punctuation
+import time
+import datetime
+from matplotlib.backends.backend_pdf import PdfPages
+
+################### fichier pdf ##############################
+pp = PdfPages(' Graphiques communes.pdf')
+firstPage = plt.figure(figsize=(11.69,8.27))
+firstPage.clf()
+txt = "Poucentage d'abstension par commune"
+firstPage.text(0.5,0.5,txt, transform=firstPage.transFigure, size=12, ha="center")
+pp.savefig()
+
+
+data = pd.read_csv('../../input_files/Presid_2017_Communes_Tour_1.csv')
+liste_communes = plt.figure()
+for i in range(1,15):
+    comm = data[data['Code du département'] == str(i)]
+    plt.bar(range(0, comm.shape[0]), comm['% Abs/Ins'].sort_values())
+    plt.title(comm['Libellé du département'].iloc[0])
+    plt.xlabel('communes')
+    plt.ylabel('% Abs/Ins')
+    pp.savefig(liste_communes)
+
+    #plt.show()
+pp.close()
