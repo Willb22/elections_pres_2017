a834140bfa36042b6a55ad13a30ed55856511cc1
will <williambaye22@gmail.com>
2020-12-03 19:20:58 +0100

    commit

--- src/communes/graphs.py
+++ src/communes/graphs.py
@@ -22,15 +22,18 @@ pp.savefig()
 
 
 data = pd.read_csv('../../data/raw/Presid_2017_Communes_Tour_1.csv')
-liste_communes = plt.figure()
-for i in range(1,15):
+fig_list = list()
+#liste_communes = plt.figure()
+for i in range(1,14):
+    fig_list.append(plt.figure())
     comm = data[data['Code du département'] == str(i)]
     plt.bar(range(0, comm.shape[0]), comm['% Abs/Ins'].sort_values())
     plt.title(comm['Libellé du département'].iloc[0])
     plt.xlabel('communes')
     plt.ylabel('% Abs/Ins')
+    pp.savefig(fig_list[i-1])
     
 
     #plt.show()
-pp.savefig(liste_communes)
+#pp.savefig(liste_communes)
 pp.close()
